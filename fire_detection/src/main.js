bdfd19e6f04d870822357b81b96eb36f2d4c103a
afnamal <afnamal@hotmail.com>
2024-07-17 21:07:14 +0300

    fixed

--- fire_detection/src/main.js
+++ fire_detection/src/main.js
@@ -1,14 +1,6 @@
-import Vue from 'vue';
-import App from './App.vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false;
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+createApp(App).mount('#app')
